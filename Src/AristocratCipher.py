1e67e42d7844dbf41283a331bb94dbaab02841e8
alexanderyw <alexanderyewang@gmail.com>
2024-01-11 13:49:40 -0500

    Added future project template and moved bugged Automated guess code for separate testing

--- Src/AristocratCipher.py
+++ Src/AristocratCipher.py
@@ -6,6 +6,7 @@ from nltk.corpus import words
 nltk.download('words')
 
 
+# Add backtrack option for hints because of potential to be wrong
 def aristocrat_menu():
     print("Aristocrat Options\n")
     while True:
@@ -60,34 +61,6 @@ class AristocratHelper:
         items = sorted(frequency_table.items(), key=lambda item: item[1], reverse=True)
         return items
 
-
-    # Still has ongoing issues
-    def generate_guess(self, target):
-        df = pd.read_csv("Src/Dependency/wordFrequency.csv")
-        frequent_words = df['word'].tolist()
-        english_words_set = set(words.words())
-
-        # Determine if a word matches the missing blanks
-        def match_pattern(word, pattern):
-            if len(str(word)) != len(pattern):
-                return False
-            for i in range(len(pattern)):
-                if pattern[i] != '_' and pattern[i] != str(word).upper()[i]:
-                    return False
-            return True
-
-        guesses = [word for word in frequent_words if match_pattern(word, target)]
-
-        if not guesses:
-            guesses = [word for word in english_words_set if match_pattern(word, target)]
-
-        if guesses:
-            # Later can implement further ordering by frequency or through context (if available)
-            random_word = random.choice(guesses)
-            return random_word
-        else:
-            print("No words matched!")
-
     def decrypt_solver(self):
         solver = self.create_empty_message()[:]
         used_letters = {}
@@ -114,6 +87,8 @@ class AristocratHelper:
                 replacement_letter = input("Enter the replacement letter: \n")
                 if letter_to_replace.isalpha() and (replacement_letter.isalpha() or replacement_letter == ""):
                     used_letters[letter_to_replace.upper()] = replacement_letter.upper()
+                    if replacement_letter == "":
+                        replacement_letter = "_"
                     solver = self.apply_letter_to_solver(solver, letter_to_replace.upper(), replacement_letter.upper())
                     self.current_message = self.apply_letter_to_solver(self.current_message, letter_to_replace.upper(),
                                                                        replacement_letter.upper())
@@ -139,24 +114,7 @@ class AristocratHelper:
 
             # Receive Automatic Guess
             elif choice == "3":
-                split_solver = solver.split()
-                word_dict = {i + 1: word for i, word in enumerate(split_solver)}
-                print(word_dict)
-                print("----------")
-                option = input("Enter the corresponding number to the word you want to guess!\n")
-                if option.isdigit() and (int(option) <= len(word_dict.keys())):
-                    guess = self.generate_guess(word_dict.get(int(option)))
-                    if guess:
-                        split_encrypt = self.encrypted_message.split()
-                        old_word = split_encrypt[int(option) - 1]
-                        for index, char in enumerate(guess):
-                            if char.isalpha():
-                                solver = self.apply_letter_to_solver(solver, old_word[index].upper(), char.upper())
-                                self.current_message = self.apply_letter_to_solver(self.current_message,
-                                                                                   old_word[index].upper(),
-                                                                                   char.upper())
-                    else:
-                        print("No matches found")
+                break
             elif choice == "4":
                 break
             elif choice == "5":
---
+++ Src/Playground/myPlayground.py
@@ -0,0 +1,64 @@
+import transformers as ppb
+
+# For DistilBERT:
+model_class, tokenizer_class, pretrained_weights = (ppb.DistilBertModel, ppb.DistilBertTokenizer, 'distilbert-base-uncased')
+
+## Want BERT instead of distilBERT? Uncomment the following line:
+#model_class, tokenizer_class, pretrained_weights = (ppb.BertModel, ppb.BertTokenizer, 'bert-base-uncased')
+
+# Load pretrained model/tokenizer
+tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
+model = model_class.from_pretrained(pretrained_weights)
+
+
+'''
+  # Still has ongoing issues
+    def generate_guess(self, target):
+        df = pd.read_csv("Src/Dependency/wordFrequency.csv")
+        frequent_words = df['word'].tolist()
+        english_words_set = set(words.words())
+
+        # Determine if a word matches the missing blanks
+        def match_pattern(word, pattern):
+            if len(str(word)) != len(pattern):
+                return False
+            for i in range(len(pattern)):
+                if pattern[i] != '_' and pattern[i] != str(word).upper()[i]:
+                    return False
+            return True
+
+        guesses = [word for word in frequent_words if match_pattern(word, target)]
+
+        if not guesses:
+            guesses = [word for word in english_words_set if match_pattern(word, target)]
+
+        if guesses:
+            # Later can implement further ordering by frequency or through context (if available)
+            random_word = random.choice(guesses)
+            return random_word
+        else:
+            print("No words matched!")
+            
+            
+                        elif choice == "3":
+                split_solver = solver.split()
+                word_dict = {i + 1: word for i, word in enumerate(split_solver)}
+                print(word_dict)
+                print("----------")
+                option = input("Enter the corresponding number to the word you want to guess!\n")
+                if option.isdigit() and (int(option) <= len(word_dict.keys())):
+                    guess = self.generate_guess(word_dict.get(int(option)))
+
+                    # Filling in the blank with the guess
+                    if guess:
+                        split_encrypt = self.encrypted_message.split()
+                        old_word = split_encrypt[int(option) - 1]
+                        for index, char in enumerate(guess):
+                            if char.isalpha():
+                                solver = self.apply_letter_to_solver(solver, old_word[index].upper(), char.upper())
+                                self.current_message = self.apply_letter_to_solver(self.current_message,
+                                                                                   old_word[index].upper(),
+                                                                                   char.upper())
+                    else:
+                    print("No matches found")
+            '''
